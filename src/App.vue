60b76a99aa52ecbe235df6e15c042818848b9a64
Valentin Dauvier <valentindauvier@hotmail.fr>
2022-04-08 18:43:21 +0200

    clone twitter frontent complet et responsive

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
             <i class="fab fa-twitter"></i>
           </button>
           <div>
-            <button v-for="tab in tabs" :key="tab.id" @click="id = tab.id" :class="`focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 ${ id === tab.id ? 'text-blue' : ''}`">
+            <button v-for="tab in tabs" :key="tab.id" @click="id = tab.id" :class="`focus:outline-none flex items-center py-2 px-4 hover:bg-lighter rounded-full mr-auto mb-3 ${ id === tab.id ? 'font-semibold' : ''}`">
               <i :class="`${ tab.icon } text-2xl mr-4 text-left`"></i>
-              <p class="text-lg font-semibold text-left hidden lg:block"> {{ tab.title }} </p>
+              <p class="text-lg text-left hidden lg:block"> {{ tab.title }} </p>
             </button>
           </div>
           <button class="text-white bg-blue rounded-full font-semibold focus:outline-none w-12 h-12 lg:h-auto lg:w-11/12 p-4 hover:bg-darkblue">
@@ -19,16 +19,16 @@
         
       <div class="lg:w-full relative">
 
-        <button @click="dropdown = true" class="flex items-center w-full hover:bg-lightblue rounded-full p-2 focus:outline-none">
+        <button @click="dropdown = true" class="flex items-center w-full hover:bg-lighter rounded-full p-3 focus:outline-none ">
           <img src="../public/img/valentin.png" class="w-10 h-10 rounded-full border-lighter" />
           <div class="hidden lg:block ml-4">
-            <p class="text-sm font-bold leading-tight"> Valentin </p>
-            <p class="text-sm leading-tight"> @VAal35 </p>
+            <p class="text-base font-bold leading-tight"> Valentin </p>
+            <p class="text-base leading-tight"> @VAal35 </p>
           </div>
           <i class="hidden lg:block ml-4 fas fa-ellipsis-h ml-auto text-lg"></i>
         </button>
 
-        <div v-if="dropdown === true" class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
+        <div v-if="dropdown === true" class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16 " >
           <button @click="dropdown = false" class="p-3 flex items-center w-full hover:bg-lightest p-2 focus:outline-none">
             <img src="../public/img/valentin.png" class="w-10 h-10 rounded-full " />
             <div class="ml-4">
@@ -62,10 +62,11 @@
           <form v-on:submit.prevent = "addNewTweet" class="w-full px-4 relative">
           <textarea v-model="tweet.content" placeholder="Quoi de neuf ?" class="mt-3 pb-3 w-full focus:outline-none"/>
           <div class="flex items-center">
-            <i class="text-lg text-blue mr-4 fa-solid fa-gif"></i>
-            <i class="text-lg text-blue mr-4 fa-solid fa-film"></i>
+            <i class="text-lg text-blue mr-4 far fa-image"></i>
+            <i class="text-lg text-blue mr-4 fas fa-film"></i>
             <i class="text-lg text-blue mr-4 far fa-chart-bar"></i>
             <i class="text-lg text-blue mr-4 far fa-smile"></i>
+            <i class="text-lg text-blue mr-4 fas fa-map-marker-alt"></i>
           </div>
           <button type="submit" class="h-10 px-4 text-white font-semibold bg-blue hover:bg-darkblue focus:outline-none rounded-full absolute bottom-0 right-0">
             Tweeter
@@ -141,16 +142,16 @@
             </div>
           </div>
         </div>
-        </div>
+      </div>
 
     </div>
   
 
     <!-- section tendances -->
 
-    <div class="md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative">
-      <input class="pl-12 rounded-full w-full p-2 bg-lighter text-sm" placeholder="Recherche Twitter" />
-      <i class="fas fa-search absolute left-0 top-0 mt-4 ml-12 text-sm text-light"></i>
+    <div class="md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6  relative">
+      <input class="pl-12 rounded-full w-full p-3 bg-lighter text-sm" placeholder="Recherche Twitter" />
+      <i class="fas fa-search absolute left-0 top-1 mt-4 ml-12 text-sm text-light"></i>
       <div class="w-full rounded-lg bg-lightest my-6">
         <div class="flex items-center justify-between p-3">
           <p class="text-lg font-bold"> Tendances pour vous </p>
@@ -187,7 +188,45 @@
         </button>
       </div>
     </div>
+
+    <div class="shadow-2xl shadow-black bg-white lg:block hidden 
+    lg:w-1/3 xl:w-1/4 2xl:w-1/5  fixed bottom-0 right-8 rounded-t-xl">
+
+        <button @click="isToggle = !isToggle" class="flex justify-between items-center w-full rounded-full p-4 focus:outline-none">
+            <p class="text-1xl font-bold leading-tight"> Messages </p>
+            <i class="far fa-envelope"></i>
+        </button>
+
+        <div v-if="isToggle === true">
+
+          <div class="flex justify-between items-center border-b p-4">
+            <p class="text-1xl leading-tight w-100">  Demandes de message </p>
+            <i class="fas fa-chevron-right"></i>
+          </div>      
+
+          <div v-for="message in messages" :key="message.id" class="w-full p-5 hover:bg-lighter flex">
+            <div class="flex-none mr-4">
+            <img :src="`${message.src}`" class="h-12 w-12 rounded-full flex-none"/>
+          </div>
+          <div class="w-full">
+            <div class="flex items-center w-full">
+              <p class="text-sm font-semibold"> {{ message.name }} </p>
+              <p class="text-sm text-dark ml-2"> {{ message.handle }} </p>
+              <p class="text-sm ml-auto"> {{ message.day }} </p>
+          </div>
+          <p class="text-sm">
+            {{ message.comments }}
+          </p>
+        </div>
+      </div>
+
+      </div>
+
+    </div>
+
   </div>
+
+
 </template>
 
 <script>
@@ -210,6 +249,7 @@ export default {
       ],
       id: 'home',
       dropdown: false,
+      isToggle: false,
       trending: [
         {top: 'Tendances en France', title: 'Benjamin', bottom: '245k tweets'},
         {top: 'Musique Tendances', title: 'Stromae', bottom: '135k Tweets'},
@@ -223,13 +263,21 @@ export default {
         {src: './img/zidane.jpg', name: 'Zidane', handle: '@Zizzou'}
       ],
       following: [
-        {src: './img/stromae.jpg', name: 'Stromae', handle: '@zizzou', time: '20 min', tweet: 'De retour sur scène !', comments: '1,000', retweets: '550', like: '10,003'},
+        {src: './img/stromae.jpg', name: 'Stromae', handle: '@stromae', time: '20 min', tweet: 'De retour sur scène !', comments: '1,000', retweets: '550', like: '10,003'},
         {src: './img/mbappe.jpg', name: 'Kylian Mbappé', handle: '@kylian17', time: '55 min', tweet: 'Merci pour tous vos messages après cette magnifique victoire', comments: '2,030', retweets: '50', like: '20,003'},
-        {src: './img/gates.jpg', name: 'Bill Gates', handle: '@billGates3', time: '1.4 hr', tweet: " J'ai sous la main une idée révolutionnaire...", comments: '1,000', retweets: '1,000', like: '205,000'},
-        {src: './img/macron.jpg', name: 'Emmanuel Macron', handle: '@teslaBoy', time: '1.4 hr', tweet: "N'oubliez pas votre devoir de citoyen. Allez voter dimanche !", comments: '1,005', retweets: '1,000', like: '105,000'}
+        {src: './img/gates.jpg', name: 'Bill Gates', handle: '@billGates3', time: '2h', tweet: " J'ai sous la main une idée révolutionnaire...", comments: '1,000', retweets: '1,000', like: '205,000'},
+        {src: './img/macron.jpg', name: 'Emmanuel Macron', handle: '@MrlePresident', time: '3h', tweet: "N'oubliez pas votre devoir de citoyen. Allez voter dimanche !", comments: '1,005', retweets: '1,000', like: '105,000'}
       ],
+      messages: [
+        {src: './img/stromae.jpg', name: 'Stromae', handle: '@stromae', day: '2 Avril', comments: 'Merci pour votre message !', },
+        {src: './img/thomas.jpg', name: 'Thomas', handle: '@totoW', day: '12 Mars', comments: 'Comment vas-tu ?', },
+        {src: './img/federer.jpg', name: 'Roger', handle: '@fedtennis', day: '8 Mars', comments: 'Petit tennis Mardi ?', },
+        {src: './img/jeanne.jpg', name: 'Jeanne', handle: '@jeaNNe', day: '27 Février', comments: 'Ok rdv samedi', },  
+        {src: './img/romain.jpg', name: 'Romain', handle: '@rominou', day: '27 Février', comments: 'Sur France Football', },      
+      ],
+
       tweets: [
-        {content: 'Vraiment cool Twitter !'}
+        {content: 'Vraiment très cool comme réseau social !'}
       ],
       tweet: {content: ''}
     }
